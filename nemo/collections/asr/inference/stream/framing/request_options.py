b3cf0c050d1f05c2234b625c0640291f761ef684
naymaraq <dkaramyan@nvidia.com>
2025-10-06 17:51:43 +0400

    [refactor: segment-level output] add output granuality to request options
    Signed-off-by: naymaraq <dkaramyan@nvidia.com>

--- nemo/collections/asr/inference/stream/framing/request_options.py
+++ nemo/collections/asr/inference/stream/framing/request_options.py
@@ -15,6 +15,7 @@
 
 from dataclasses import dataclass
 from typing import TypeAlias
+from nemo.collections.asr.inference.utils.enums import ASROutputGranularity
 
 
 @dataclass(frozen=True, slots=True)
@@ -27,6 +28,55 @@ class ASRRequestOptions:
     enable_itn: bool = None
     enable_pnc: bool = None
     stop_history_eou: int = None
+    asr_output_granularity: ASROutputGranularity | str = None
+
+    def __post_init__(self) -> None:
+        """
+        Post-init hook:
+            Converts the asr_output_granularity to ASROutputGranularity if it is a string
+        """
+        if isinstance(self.asr_output_granularity, str):
+            self.asr_output_granularity = ASROutputGranularity.from_str(self.asr_output_granularity)
+
+    def is_word_level_output(self) -> bool:
+        """
+        Check if the output granularity is word level.
+        """
+        return self.asr_output_granularity is ASROutputGranularity.WORD
+
+    def is_segment_level_output(self) -> bool:
+        """
+        Check if the output granularity is segment level.
+        """
+        return self.asr_output_granularity is ASROutputGranularity.SEGMENT
+
+    def augment_with_defaults(
+        self,
+        default_enable_itn: bool,
+        default_enable_pnc: bool,
+        default_stop_history_eou: int,
+        default_asr_output_granularity: ASROutputGranularity | str,
+    ) -> "ASRRequestOptions":
+        """
+        Augment the options with the default values.
+        Args:
+            default_enable_itn (bool): Default enable ITN.
+            default_enable_pnc (bool): Default enable PNC.
+            default_stop_history_eou (int): Default stop history EOU.
+            default_asr_output_granularity (ASROutputGranularity | str): Default output granularity.
+        Returns:
+            ASRRequestOptions: Augmented options.
+        """
+        if isinstance(default_asr_output_granularity, str):
+            default_asr_output_granularity = ASROutputGranularity.from_str(default_asr_output_granularity)
+        return ASRRequestOptions(
+            enable_itn=default_enable_itn if self.enable_itn is None else self.enable_itn,
+            enable_pnc=default_enable_pnc if self.enable_pnc is None else self.enable_pnc,
+            stop_history_eou=default_stop_history_eou if self.stop_history_eou is None else self.stop_history_eou,
+            asr_output_granularity=(
+                default_asr_output_granularity if self.asr_output_granularity is None else self.asr_output_granularity
+            ),
+        )
 
 
 RequestOptions: TypeAlias = ASRRequestOptions
