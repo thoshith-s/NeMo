a69f1864e5631f375e96ef686e53d1d5b54dad9d
dimapihtar <dimapihtar@users.noreply.github.com>
2025-10-15 12:25:42 +0000

    Apply isort and black reformatting
    Signed-off-by: dimapihtar <dimapihtar@users.noreply.github.com>

--- nemo/collections/multimodal/speech_llm/modules/common/text_generation_utils.py
+++ nemo/collections/multimodal/speech_llm/modules/common/text_generation_utils.py
@@ -12,10 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import numpy as np
 import torch
 import torch.nn.functional as F
-
-import numpy as np
 
 from nemo.utils import AppState
 
