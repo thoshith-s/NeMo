5a7a1ce2313063b374ef57bb726faea04845fdeb
naymaraq <dkaramyan@nvidia.com>
2025-10-15 14:43:39 +0400

    add CI script for testing
    Signed-off-by: naymaraq <dkaramyan@nvidia.com>

---
+++ tests/functional_tests/L2_Speech_Transcription_Streaming_Inference.sh
@@ -0,0 +1,52 @@
+# Copyright (c) 2020-2025, NVIDIA CORPORATION.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+coverage run -a --data-file=/workspace/.coverage --source=/workspace/nemo examples/asr/asr_chunked_inference/asr_streaming_infer.py \
+    --config-path=examples/asr/conf/asr_chunked_inference/ \
+    --config-name=buffered_ctc.yaml \
+    audio_file="/home/TestData/an4_transcribe/test_subset/" \
+    output_filename="/tmp/buffered_ctc_test_res.json" \
+    lang=en \
+    enable_pnc=False \
+    enable_itn=False \
+    asr_output_granularity=segment
+
+coverage run -a --data-file=/workspace/.coverage --source=/workspace/nemo examples/asr/asr_chunked_inference/asr_streaming_infer.py \
+    --config-path=examples/asr/conf/asr_chunked_inference/ \
+    --config-name=buffered_rnnt.yaml \
+    audio_file="/home/TestData/an4_transcribe/test_subset/" \
+    output_filename="/tmp/buffered_rnnt_test_res.json" \
+    lang=en \
+    enable_pnc=False \
+    enable_itn=False \
+    asr_output_granularity=segment
+
+coverage run -a --data-file=/workspace/.coverage --source=/workspace/nemo examples/asr/asr_chunked_inference/asr_streaming_infer.py \
+    --config-path=examples/asr/conf/asr_chunked_inference/ \
+    --config-name=cache_aware_ctc.yaml \
+    audio_file="/home/TestData/an4_transcribe/test_subset/" \
+    output_filename="/tmp/cache_aware_ctc_test_res.json" \
+    lang=en \
+    enable_pnc=False \
+    enable_itn=False \
+    asr_output_granularity=segment
+
+coverage run -a --data-file=/workspace/.coverage --source=/workspace/nemo examples/asr/asr_chunked_inference/asr_streaming_infer.py \
+    --config-path=examples/asr/conf/asr_chunked_inference/ \
+    --config-name=cache_aware_rnnt.yaml \
+    audio_file="/home/TestData/an4_transcribe/test_subset/" \
+    output_filename="/tmp/cache_aware_rnnt_test_res.json" \
+    lang=en \
+    enable_pnc=False \
+    enable_itn=False \
+    asr_output_granularity=segment
